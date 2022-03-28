002f353d0f5c5762133d297bc8880bf2f55f0382
emandybura <emandybura@yandex.ru>
2022-03-28 13:14:09 +0500

    First comm

--- project_0/Game.py
+++ project_0/Game.py
@@ -1,18 +1,22 @@
-from itertools import count
+"""Игра угадай число"""
+
 import numpy as np
 
-number=np.random.randint(1,101)
-count=0
+number = np.random.randint(1, 101) # загадываем число
+
+# количество попыток
+count = 0
+
 while True:
     count+=1
-    predict_number=int(input("Угадай число от 1 до 100 "))
+    predict_number = int(input("Угадай число от 1 до 100: "))
     
-    if predict_number>number:
+    if predict_number > number:
         print("Число должно быть меньше!")
-        
-    elif predict_number<number:
+
+    elif predict_number < number:
         print("Число должно быть больше!")
-        
+    
     else:
         print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break
+        break #конец игры выход из цикла
